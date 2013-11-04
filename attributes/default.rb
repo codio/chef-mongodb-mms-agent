f4818e96393c9a513871541571aadcb2a90de7ef
Joel Moss <joel@developwithstyle.com>
2013-11-04 15:29:24 +0000

    Using symbols in attribute keys

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['mms_agent']['api_key'] = ''
-default['mms_agent']['secret_key'] = ''
+default[:mms_agent][:api_key] = ''
+default[:mms_agent][:secret_key] = ''
 
-default['mms_agent']['user'] = 'mmsagent'
-default['mms_agent']['group'] = 'mmsagent'
+default[:mms_agent][:user] = 'mmsagent'
+default[:mms_agent][:group] = 'mmsagent'
--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "jmoss@codio.com"
 license          "All rights reserved"
 description      "Installs/Configures MongoDB mms-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 depends 'python'
