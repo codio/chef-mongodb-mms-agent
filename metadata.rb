14ff618cee36fddd210e22bbe7ca968077b69f6d
Joel Moss <joel@developwithstyle.com>
2013-11-04 12:07:00 +0000

    Bumped to version 1.0 and removed base dep.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer_email "jmoss@codio.com"
 license          "All rights reserved"
 description      "Installs/Configures MongoDB mms-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "1.0.0"
 
 depends 'python'
-depends 'base', '~> 0.3.0'
