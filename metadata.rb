6448145faa2121632b894713123ec961d06ce9dd
Joel Moss <joel@developwithstyle.com>
2014-01-08 16:53:14 +0000

    Bumped base dep

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,10 @@ maintainer_email "jmoss@codio.com"
 license          "All rights reserved"
 description      "Installs/Configures MongoDB mms-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.6.0"
+version          "1.6.1"
 
-depends 'base', '1.2.1'
+depends 'base', '1.2.2'
 depends 'python'
-depends 'chef-sugar'
 
 recipe 'mongodb-mms-agent::default', 'Installs and configures the MongoDB MMS agent'
 recipe 'mongodb-mms-agent::backup',  'Installs and configures the MongoDB MMS Backup agent'
