170d90b8f7381b0cc08b2426240e6b9c6b80b11a
Joel Moss <joel@developwithstyle.com>
2014-01-09 00:00:20 +0000

    Removed base cookbook version lock

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "jmoss@codio.com"
 license          "All rights reserved"
 description      "Installs/Configures MongoDB mms-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.6.1"
+version          "1.6.2"
 
-depends 'base', '1.2.2'
+depends 'base'
 depends 'python'
 
 recipe 'mongodb-mms-agent::default', 'Installs and configures the MongoDB MMS agent'
